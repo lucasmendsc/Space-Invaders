7ab88832d355c646b791cf77682aee6a6f737687
Lucas Mendes <lucas.mendes147@live.com>
2018-06-29 01:12:25 -0300

    Corrigido movimento da nave e adicionada colisões aos inimigos

--- EnemyShips.lua
+++ EnemyShips.lua
@@ -23,25 +23,114 @@ function enemyShips:setShips()
         y = 10
     end 
 end
-
 enemyShips:setShips()
+function shipColisionListiners()
 
+    local function ship1ColisionListiner()
+        display.remove(enemyShips[1][1])
+    end
+
+    local function ship2ColisionListiner()
+        display.remove(enemyShips[1][2])
+    end
+
+    local function ship3ColisionListiner()
+        display.remove(enemyShips[1][3])
+    end
+
+    local function ship4ColisionListiner()
+        display.remove(enemyShips[1][4])
+    end
+
+    local function ship5ColisionListiner()
+        display.remove(enemyShips[2][1])
+    end
+
+    local function ship6ColisionListiner()
+        display.remove(enemyShips[2][2])
+    end
+
+    local function ship7ColisionListiner()
+        display.remove(enemyShips[2][3])
+    end
+
+
+    local function ship8ColisionListiner()
+        display.remove(enemyShips[2][4])
+    end
+
+    local function ship9ColisionListiner()
+        display.remove(enemyShips[3][1])
+    end
+
+    local function ship10ColisionListiner()
+        display.remove(enemyShips[3][2])
+    end
+
+    local function ship11ColisionListiner()
+        display.remove(enemyShips[3][3])
+    end
+
+    local function ship12ColisionListiner()
+        display.remove(enemyShips[3][4])
+    end
+
+    local function ship13ColisionListiner()
+        display.remove(enemyShips[4][1])
+    end
+
+    local function ship14ColisionListiner()
+        display.remove(enemyShips[4][2])
+    end
+
+     local function ship15ColisionListiner()
+        display.remove(enemyShips[4][3])
+    end
+
+     local function ship16ColisionListiner()
+        display.remove(enemyShips[4][4])
+    end
+
+    enemyShips[1][1]:addEventListener( "collision", ship1ColisionListiner )
+    enemyShips[1][2]:addEventListener( "collision", ship2ColisionListiner )
+    enemyShips[1][3]:addEventListener( "collision", ship3ColisionListiner )
+    enemyShips[1][4]:addEventListener( "collision", ship4ColisionListiner )
+    enemyShips[2][1]:addEventListener( "collision", ship5ColisionListiner )
+    enemyShips[2][2]:addEventListener( "collision", ship6ColisionListiner )
+    enemyShips[2][3]:addEventListener( "collision", ship7ColisionListiner )
+    enemyShips[2][4]:addEventListener( "collision", ship8ColisionListiner )
+    enemyShips[3][1]:addEventListener( "collision", ship9ColisionListiner )
+    enemyShips[3][2]:addEventListener( "collision", ship10ColisionListiner )
+    enemyShips[3][3]:addEventListener( "collision", ship11ColisionListiner )
+    enemyShips[3][4]:addEventListener( "collision", ship12ColisionListiner )
+    enemyShips[4][1]:addEventListener( "collision", ship13ColisionListiner )
+    enemyShips[4][2]:addEventListener( "collision", ship14ColisionListiner )
+    enemyShips[4][3]:addEventListener( "collision", ship15ColisionListiner )
+    enemyShips[4][4]:addEventListener( "collision", ship16ColisionListiner )
+
+end
+
+
+shipColisionListiners()
 function enemyShips:addPhysic()
     for i=1,4 do
         for j=1,4 do
             physics.addBody(enemyShips[i][j],"dinamyc")
+            enemyShips[i][j].gravityScale = 0
         end
     end
 
 end
-
+physics.start()
 enemyShips:addPhysic()
 
 function enemyShips:moveShips()
     local function moveLeft()
         for i=1,4 do
             for j=1,4 do
-                enemyShips[i][j].x = enemyShips[i][j].x + 5
+                if enemyShips[i][j].x ~= nil then
+                    enemyShips[i][j].x = enemyShips[i][j].x + 10
+                end
             end
         end
     end
--- Player.lua
+++ Player.lua
@@ -1,29 +1,40 @@
 local physics = require("physics")
 player = {lifes = 3, 
-        playerShip = display.newImage("Game Images/playerShip.png",160,460)
+        playerShip = display.newImage("Game Images/playerShip.png",160,460),
+        shots ={}
         }
 
 
 local function onAccelerate(event)
     player.playerShip.x = display.contentCenterX + 
         (display.contentCenterX * (event.xGravity*2)) 
-end
 
-rect = display.newRect(20,20,50,50)
+    if player.playerShip.x > 295 then
+        player.playerShip.x = 295
+    elseif player.playerShip.x < 25 then
+        player.playerShip.x = 25
+    end
+end
 
 function shoot( event )
     if event.phase == "began" then
-        shot = player:createBullet()
+       shot = player:createBullet()
 
             local function moveShot()
                 shot.y = shot.y - 5
+                removeShot(shot)
             end
-
         timerPlayerShipShot = timer.performWithDelay(10,moveShot,0)
     end
 end
 
-rect:addEventListener("touch",shoot)
+function removeShot(shot)
+  --[[]  if shot.y == 25 then
+        display.remove(shot)
+    end]]
+end
+
+Runtime:addEventListener("touch",shoot)
 
 function onShipCollision(event)
     transition.fadeOut( player.playerShip, { time=500 })
@@ -33,6 +44,7 @@ end
 function player:onStartFunctions()
     physics.start()
     physics.addBody(player.playerShip,"static")
+end
 
 function onShotCollision()
     display.remove(shot)
@@ -41,10 +53,12 @@ end
     player.playerShip:addEventListener( "collision", onShipCollision )
     system.setAccelerometerInterval( 60 )
     Runtime:addEventListener ("accelerometer", onAccelerate)
-end
 
 function player:createBullet()
-    return display.newRect(player.playerShip.x,player.playerShip.y - 35,5,25)
+    shot = display.newRect(player.playerShip.x,player.playerShip.y -40 , 5, 25)
+    physics.addBody(shot,"dynamic")
+    shot.gravityScale = 0
+    return shot
 end
 
 function player:ableToShoot()
